5ed78b61c206244dbc36af26a2f1d7e283c66ab6
Evgeniy Matveev <mfb.eugene@gmail.com>
2020-05-23 23:03:17 +0300

    deleted notifications.js

--- src/notifications.js
+++
@@ -1,8 +0,0 @@
-import notification from "ant-design-vue";
-
-openNotificationWithIcon(type, message, description) {
-    notification[type]({
-      message: message,
-      description: description
-    })
-};
