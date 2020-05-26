fda3b8afaac9aaa8b539567fe67ca0b541df76e8
Evgeniy Matveev <mfb.eugene@gmail.com>
2020-05-27 01:40:03 +0300

    menu icons

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -10,42 +10,42 @@
       :inline-collapsed="collapsed"
     >
       <a-menu-item key="1">
-        <a-icon type="pie-chart" />
+        <a-icon type="profile" />
         <span>Journal</span>
         <router-link to="/"></router-link>
       </a-menu-item>
       <a-menu-item key="2">
-        <a-icon type="pie-chart" />
+        <a-icon type="read" />
         <span>Book</span>
         <router-link to="/Book"></router-link>
       </a-menu-item>
       <a-menu-item key="3">
-        <a-icon type="pie-chart" />
+        <a-icon type="idcard" />
         <span>Reader</span>
         <router-link to="/Reader"></router-link>
       </a-menu-item>
       <a-menu-item key="4">
-        <a-icon type="pie-chart" />
+        <a-icon type="user" />
         <span>Author</span>
         <router-link to="/Author"></router-link>
       </a-menu-item>
       <a-menu-item key="5">
-        <a-icon type="pie-chart" />
+        <a-icon type="global" />
         <span>City</span>
         <router-link to="/City"></router-link>
       </a-menu-item>
       <a-menu-item key="6">
-        <a-icon type="pie-chart" />
+        <a-icon type="book" />
         <span>Edition</span>
         <router-link to="/Edition"></router-link>
       </a-menu-item>
       <a-menu-item key="7">
-        <a-icon type="pie-chart" />
+        <a-icon type="home" />
         <span>Library</span>
         <router-link to="/Library"></router-link>
       </a-menu-item>
       <a-menu-item key="8">
-        <a-icon type="pie-chart" />
+        <a-icon type="team" />
         <span>Publisher</span>
         <router-link to="/Publisher"></router-link>
       </a-menu-item>
