753490a3a54731dbdb038baa3a3a43d2e4fadfff
Evgeniy Matveev <mfb.eugene@gmail.com>
2020-05-26 21:21:56 +0300

    new database

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const pg = require('pg');
 const config = {
-    user: "fqxsfzdrnznmir",
-    password: "f5f8a516df29ac15d0b95c2dfdaee480511c60b9ff5819bc7d62c7cb13f3d497",
-    database: "d7pvv3v5i5cqf2",
+    user: "edbfiupkxewdpa",
+    password: "7654bc069da1e6b889d586e0bf023c6632d2445c4c6977009859ec0db694fceb",
+    database: "d15aohh251nhi9",
     port: 5432,
-    host: "ec2-46-137-100-204.eu-west-1.compute.amazonaws.com",
+    host: "ec2-54-246-90-10.eu-west-1.compute.amazonaws.com",
     ssl: true
 };
 
